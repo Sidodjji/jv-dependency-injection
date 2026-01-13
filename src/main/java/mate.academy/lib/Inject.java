0b3a3240315a046c41d25905c02e744a12d161ed
Sidodji <andreymaluk35@gmail.com>
2026-01-13 02:30:55 +0200

    Fix imports

--- src/main/java/mate.academy/lib/Inject.java
+++ src/main/java/mate.academy/lib/Inject.java
@@ -2,8 +2,9 @@ package mate.academy.lib;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
